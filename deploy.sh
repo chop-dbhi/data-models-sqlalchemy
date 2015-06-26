54e60082a8971f1e12f5fdd965c633e19abcf6b5
Aaron Browne <aaron0browne@gmail.com>
2015-06-26 18:27:07 -0400

    Tweak Circle CI deployment
    Signed-off-by: Aaron Browne <aaron0browne@gmail.com>

--- deploy.sh
+++ deploy.sh
@@ -3,27 +3,30 @@
 VERSION=$(./version.sh)
 EB_BUCKET=elasticbeanstalk-us-east-1-248182584102
 
-# Deploy image to Docker Hub
+# Push image to Docker Hub registry.
 docker login -e $DOCKER_EMAIL -u $DOCKER_USER -p $DOCKER_PASS
-docker push dbhi/data-models-sqlalchemy:$VERSION
+docker push dbhi/data-models-sqlalchemy:$(./version.sh | sed "s/+/-/")
 
-# Upload package to PyPi
-sed -e "s/<PYPI_USER>/$PYPI_USER/" -e "s/<PYPI_PASS>/$PYPI_PASS/" \
-    < .pypirc.template > .pypirc
-python setup.py register
-python setup.py sdist bdist_wheel
-twine upload dist/*
+# Upload package to PyPi if final release.
+if [ ${#VERSION} -lt 6 ]; then
+    pip install wheel twine
+    sed -e "s/<PYPI_USER>/$PYPI_USER/" -e "s/<PYPI_PASS>/$PYPI_PASS/" \
+        < .pypirc.template > .pypirc
+    python setup.py register
+    python setup.py sdist bdist_wheel
+    twine upload -u $PYPI_USER -p $PYPI_PASS dist/*
+fi
 
-# Create new Elastic Beanstalk version
+# Create new Elastic Beanstalk version.
 DOCKERRUN_FILE=$VERSION-Dockerrun.aws.json
 sed "s/<TAG>/$VERSION/" < Dockerrun.aws.json.template > $DOCKERRUN_FILE
-aws s3 cp $DOCKERRUN_FILE s3://$EB_BUCKET/$DOCKERRUN_FILE
-aws elasticbeanstalk create-application-version \
+aws --region=us-east-1 s3 cp $DOCKERRUN_FILE s3://$EB_BUCKET/$DOCKERRUN_FILE
+aws --region=us-east-1 elasticbeanstalk create-application-version \
     --application-name data-models-sqlalchemy \
     --version-label $VERSION \
     --source-bundle S3Bucket=$EB_BUCKET,S3Key=$DOCKERRUN_FILE
 
-# Update Elastic Beanstalk environment to new version
-aws elasticbeanstalk update-environment \
-    --environment-name data-model-sqlalchemy \
+# Update Elastic Beanstalk environment to new version.
+aws --region=us-east-1 elasticbeanstalk update-environment \
+    --environment-name data-models-sqlalchemy \
     --version-label $VERSION
