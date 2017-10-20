2b82df61a0c9a3df31f74fa6623be538b44fbb79
Aaron Browne <aaron0browne@gmail.com>
2017-10-20 14:40:13 -0400

    Document Terraform deployment and remove deploy configuration (#53)
    Signed-off-by: Aaron Browne <aaron0browne@gmail.com>

--- ci/init.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Stop the ECS service from trying to join a cluster.
-stop ecs || true && mv /etc/init/ecs.conf /etc/init/ecs.conf.disabled
-
-# Pull and run the service.
-docker pull dbhi/data-models-sqlalchemy:${tag}
-docker run -d -p "80:80" dbhi/data-models-sqlalchemy:${tag}
